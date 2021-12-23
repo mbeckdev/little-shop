d03ecf538012da418306f42f6c004221c0b1c689
mbeckdev <mbeckdev42@gmail.com>
2021-12-23 16:37:31 -0700

    Add shopping icon div structure for number in cart

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,18 +2,24 @@ import React from 'react';
 import styles from './shop.module.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faShoppingCart } from '@fortawesome/fontawesome-free-solid';
 
 const Shop = () => {
   return (
     <div className={styles.shop}>
       <div className={styles.shoppingCartIconBar}>
-        <div>shopping cart icon</div>
-
-        <FontAwesomeIcon icon={faShoppingCart} />
+        <div className={styles.iconContainer}>
+          <div id={styles.numberInCart}>24</div>
+          <div className={styles.shoppingCartIcon}>
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              size="2x"
+              className={styles.actualIcon}
+            />
+          </div>
+        </div>
       </div>
-      <div>shop items here</div>
+      <div className={styles.shopItems}>shop items here</div>
     </div>
   );
 };
