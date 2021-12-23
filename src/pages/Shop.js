49a44e8e857ece19491244b9e7229710e5cde848
mbeckdev <mbeckdev42@gmail.com>
2021-12-23 14:21:26 -0700

    Add font-awesome cart icon

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './shop.module.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faShoppingCart } from '@fortawesome/fontawesome-free-solid';
 
 const Shop = () => {
-  return <div>Shop here</div>;
+  return (
+    <div className={styles.shop}>
+      <div className={styles.shoppingCartIconBar}>
+        <div>shopping cart icon</div>
+
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </div>
+      <div>shop items here</div>
+    </div>
+  );
 };
 
 export default Shop;
