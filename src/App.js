cd6ba8ee5f219fb9a5869a48ef97ffe74bd3b04a
mbeckdev <mbeckdev42@gmail.com>
2021-12-22 18:05:05 -0700

    Add initial components and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import React from 'react';
+// import App from './App';
 import './App.css';
+import Navbar from './Navbar';
+import Shop from './pages/Shop';
+import Home from './pages/Home';
 
-function App() {
-  return <div className="App">hiya</div>;
-}
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          {/* <Route index element={<Navbar />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App;
+export default RouteSwitch;
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul className="nav-links">
+        <Link to="/home">
+          <li>Home</li>
+        </Link>
+        <Link to="/shop">
+          <li>Shop</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
