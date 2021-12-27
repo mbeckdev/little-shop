ae9ce3a769c1a70585404915a402e5884b60dc0e
mbeckdev <mbeckdev42@gmail.com>
2021-12-27 13:06:37 -0700

    Add homepage picture and text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './home.module.css';
 
 const Home = () => {
-  return <div>Home here</div>;
+  return (
+    <div id={styles.home}>
+      <div id={styles.titleContainer}>
+        <h1>Mini Jungle</h1>
+        <p>Make it look like a jungle wherever you are</p>
+        <button>Shop Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
