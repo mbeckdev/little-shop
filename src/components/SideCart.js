0ed416c2a0aa135d48c8aadb3617ec93dbcf50e3
mbeckdev <mbeckdev42@gmail.com>
2022-01-10 12:39:28 -0700

    Add side cart layout styling

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -9,8 +9,35 @@ function SideCart({ cart }) {
 
       <div className={styles.sideCartItemContainer}>
         {cart.items.map((itemInCart) => (
-          <div key={itemInCart.id}>
+          <div key={itemInCart.id} className={styles.sideCartItem}>
             qty {itemInCart.qty} id: {itemInCart.id}
+            <div className={styles.sideCartSectionA}>
+              <img src={itemInCart.img} alt={itemInCart.altText} />
+              {/* <img src={imgPath} alt={item.altText} /> */}
+              <div className={styles.sideCartSectionA_A}>
+                <div className={styles.shopItemTitle}>{itemInCart.name}</div>
+                <div className={styles.shopItemPrice}>${itemInCart.price}</div>
+              </div>
+            </div>
+            <div className={styles.sideCartSectionB}>
+              <div className={styles.sideCartSectionB_qtyContainer}>
+                <button>-</button>
+                <div className={styles.inputContainer}>
+                  <input type="text" />
+                </div>
+                <button>+</button>
+              </div>
+              <button>Delete</button>
+            </div>
+            {/* <button
+              className="btn"
+              onClick={(e) => {
+                handleAddToCart(e, item);
+              }}
+            >
+              <button className="btn" onClick={handleAddToCart}>
+              Add to Cart
+            </button> */}
           </div>
         ))}
       </div>
